b773605039fd2619d186ac0b75d2f409f47d5079
akshay-raj-dhamija <akshay.raj.dhamija@gmail.com>
2020-07-30 16:32:03 -0600

    more cleanup and testing scripts

--- weibull.py
+++ weibull.py
@@ -1,8 +1,6 @@
 import numpy as np
 import torch
-from weibull.backend_pytorch import fit
-import timeit
-
+from backend_weibull import fit
 
 def weibullFitting(dataTensor, tailSize, sign, isSorted = 0):
   
@@ -23,6 +21,7 @@ def weibullFitting(dataTensor, tailSize, sign, isSorted = 0):
   processedTensor = sortedTensor + trnaslateAmoutTensor - smallScoreTensor
   wbFits = fit(processedTensor)
   
+  print ("signTensor",wbFits.dtype,signTensor.dtype,smallScoreTensor.dtype)
   resultTensor = torch.cat([wbFits[:,1].unsqueeze(1),wbFits[:,0].unsqueeze(1),signTensor,trnaslateAmoutTensor,smallScoreTensor], dim=1)
 
   return resultTensor
@@ -30,36 +29,5 @@ def weibullFitting(dataTensor, tailSize, sign, isSorted = 0):
 def FitLow(data, tailSize, isSorted = 0):
   return weibullFitting(data, tailSize, -1, isSorted)
   
-  
 def FitHigh(data, tailSize, isSorted = 0):
   return weibullFitting(data, tailSize, 1, isSorted)
-  
-
-def test_weibullFit():
-  fileName = 'sample_data/weibulls_example_protocol2.npy'
-  data = np.load(fileName, allow_pickle=True)
-  
-  numInstances = 10
-  dataSize = 40000
-  tailSize = 2500
-  distance_multiplier = 0.5
-  
-  data1 = np.zeros(shape=(numInstances,dataSize), dtype=np.float64)
-  weibull_fits_libmr = np.zeros(shape=(numInstances,5), dtype=np.float64)
-  
-  for k in range(numInstances):
-    data1[k,:] = distance_multiplier * data[k][0]
-    weibull_fits_libmr[k,:] = data[k][1]
-  
-  dataTensor = torch.from_numpy(data1)
-  dataTensor = dataTensor.cuda()
-  
-  result = FitLow(dataTensor, tailSize, 0)
-  
-  print(result)
-  
-  return
-  
-if __name__ == '__main__':
-  test_weibullFit()
-  
